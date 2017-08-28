8c20754ac9860329874a85ad02a6fcf05020cac2
Raspy 94 <31358813+Raspy94@users.noreply.github.com>
2017-08-28 14:06:01 +0100

    Update flight.py

--- flight.py
+++ flight.py
@@ -5,25 +5,33 @@ import math
 import requests
 import re
 
-url1='http://192.168.1.12:8080/data.json'
+url1='http://yourIPaddress:8080/data.json'
 
 try:
  x=[]
  y=[]
  distance=[]
+ 
+ #get list of planes seen from your radar
  flight=urlopen(url1).read()
  flight=flight.decode('utf-8')
  results=json.loads(flight)
+ 
+ #get coordinates of all the planes and store them in a lists of tuples
  for i in results:
   lat = i.get("lat")
   lon = i.get("lon")
   x.append(lat)
   y.append(lon)
  z = zip(x,y)
- me=(51.184708,0.268231)
+ me=(51.184708,0.268231) #use your coordinates here
+ 
+ #calculate linear distances between you and each plane and store them in a list
  for j in z:
   dist=math.hypot(abs(j[0] - me[0]), abs(j[1] - me[1])) # Linear distance 
   distance.append(dist)
+ 
+ #get the index of the plane closer to you and print its coordinates
  index_min = min(xrange(len(distance)), key=distance.__getitem__)
  closer = z[index_min]
  x0=closer[0]
@@ -31,6 +39,10 @@ try:
  x0=str(x0)
  x1=str(x1)
  print x0, x1
+ 
+ #get the details of 'your plane'from public-api.adsbexchange.com 
+ #it could be more precise using the icao of the plane instead of the coordinates
+ #for more details look here http://www.virtualradarserver.co.uk/Documentation/Formats/AircraftList.aspx#response 
  try:
   url2="https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat="+ x0 + "&lng=" + x1 + "&fDstL=0&fDstU=5"
   data = requests.get(url2).json()
